28bebc8eed6af9999d149714fa1503c5c1f13aaf
nurul laili <nurullailibintimohdzawawi@gmail.com>
2025-11-26 17:23:10 +0800

    change

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -140,11 +140,11 @@ export default function DownloadPage() {
       });
 
       // Start download from Google Drive
-      window.location.href = 'https://drive.google.com/uc?export=download&id=1ZdvQt24NCcS974KidVzHKDIaQ-tv5ZFx';
+      window.location.href = 'https://drive.google.com/uc?export=download&id=1bg7gKOkU9x3ZnI2ac9P3WWln6XMAPaiB';
     } catch (error) {
       console.error('Error logging download:', error);
       // Still allow download even if logging fails
-      window.location.href = 'https://drive.google.com/uc?export=download&id=1ZdvQt24NCcS974KidVzHKDIaQ-tv5ZFx';
+      window.location.href = 'https://drive.google.com/uc?export=download&id=1bg7gKOkU9x3ZnI2ac9P3WWln6XMAPaiB';
     } finally {
       setTimeout(() => setIsDownloading(false), 2000);
     }
